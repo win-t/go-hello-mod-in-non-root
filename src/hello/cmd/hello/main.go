fa4c0275644070d2c7e1d1e299db18a95e86712c
Kurnia D Win <kurnia.d.win@gmail.com>
2023-04-17 06:18:20 +0700

    init

---
+++ src/hello/cmd/hello/main.go
@@ -0,0 +1,7 @@
+package main
+
+import "github.com/win-t/go-hello-mod-in-non-root/src/hello/pkg/hello"
+
+func main() {
+	hello.SayHello()
+}
---
+++ src/hello/go.mod
@@ -0,0 +1,3 @@
+module github.com/win-t/go-hello-mod-in-non-root/src/hello
+
+go 1.20
